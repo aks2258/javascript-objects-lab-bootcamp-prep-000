0513cfebeaa46b8e7cfea351a69592c574a54fa8
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 00:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function dele  delete newObj[key]
-  return newObj
-}ctFromObjectByKey(object, key){
+function deleteFromObjectByKey(object, key){
   var newObj=Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
